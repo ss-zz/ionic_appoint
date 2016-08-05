4d0a7f86959560fbcf5e69dce6aa7a8aa96875f9
miao <641105850@qq.com>
2016-08-05 13:59:23 +0800

    医生详情页面的实现

--- www/js/src/doctor/controllers/DoctorDetailCtrl.js
+++ www/js/src/doctor/controllers/DoctorDetailCtrl.js
@@ -6,18 +6,13 @@ app.controller('DoctorDetailCtrl', function($scope, $stateParams,$state, DoctorS
 	
 	//$scope.hos = hos;
 	$scope.allJobs = [[], [], [], []];
-	$scope.fullJobs = [];//全班
-	$scope.moningJobs = [];//上午班
-	$scope.afternoonJobs = [];//下午班
-	$scope.nightJobs = [];//晚班
+	//$scope.fullJobs = [];//全班
+	//$scope.moningJobs = [];//上午班
+	//$scope.afternoonJobs = [];//下午班
+	//$scope.nightJobs = [];//晚班
 	$scope.currentDate = null;
     var dayindex=0;
-    var list=[
-        {"name":"A","Id":1},
-        {"name":"B","Id":2},
-        {"name":"C","Id":3}
-    ];
-    $scope.Names1 = [{"name":"晚上"},{"name":" "},{"name":" "},{"name":" "},{"name":" "},{"name":" "},{"name":" "},{"name":" "}];
+    //$scope.Names1 = [{"name":"晚上"},{"name":" "},{"name":" "},{"name":" "},{"name":" "},{"name":" "},{"name":" "},{"name":" "}];
     var weekNames = ['周日','周一','周二','周三','周四','周五','周六'];
 	var now = new Date();
 	var DAY_Millisecond = 86400000;
@@ -48,21 +43,126 @@ app.controller('DoctorDetailCtrl', function($scope, $stateParams,$state, DoctorS
 	$scope.docId = $scope.doctor.doctorId;
 	getArrayJob();
 	set();
-	//addclick();
+	addclick();
 	
 	//添加事件代理
-//	function addclick(){
-//		// 获取父节点，并为它添加一个click事件
-// document.getElementById("three").addEventListener("click",function(e) {
-//  // 真正的处理过程在这里
-//  e.target.id
-//  console.log("List item ",e.target.id.replace("post-")+"--"+e.target.id," was clicked!");
-//  if(e.target.id.replace("post-")=="p4"){
-//  	goAppoint1($scope.pi4);
+	function addclick(){
+		// 获取父节点，并为它添加一个click事件
+   document.getElementById("three").addEventListener("click",function(e) {
+    // 真正的处理过程在这里
+ 
+    console.log("List item ",e.target.id.replace("post-")+"--"+e.target.parentElement.id," was clicked!");
+//  if(e.target.parentElement.id=="p5"){
+//  	goAppoint1($scope.pi5);
 //  }
-//
-//});
-//	}
+    switch (e.target.parentElement.id){
+    	
+    		case "p1":
+    	goAppoint1($scope.pi1);
+    	break;
+    		case "p2":
+    	goAppoint1($scope.pi2);
+    	break;
+    		case "p3":
+    	goAppoint1($scope.pi3);
+    	break;
+    		case "p4":
+    	goAppoint1($scope.pi4);
+    	break;
+    	case "p5":
+    	goAppoint1($scope.pi5);
+    	break;
+    		case "p6":
+    	goAppoint1($scope.pi6);
+    	break;
+    		case "p7":
+    	goAppoint1($scope.pi7);
+    	break;
+    }
+
+});
+document.getElementById("one").addEventListener("click",function(e) {
+    // 真正的处理过程在这里
+    switch (e.target.parentElement.id){
+    	case "q1":
+    	goAppoint1($scope.qi1);
+    	break;
+    		case "q2":
+    	goAppoint1($scope.qi2);
+    	break;
+    		case "q3":
+    	goAppoint1($scope.qi3);
+    	break;
+    		case "q4":
+    	goAppoint1($scope.qi4);
+    	break;
+    		case "q5":
+    	goAppoint1($scope.qi5);
+    	break;
+    		case "q6":
+    	goAppoint1($scope.qi6);
+    	break;
+    		case "q7":
+    	goAppoint1($scope.qi7);
+    	break;
+    }
+
+});
+document.getElementById("two").addEventListener("click",function(e) {
+    // 真正的处理过程在这里
+    switch (e.target.parentElement.id){
+    case "a1":
+    	goAppoint1($scope.ai1);
+    	break;
+    		case "a2":
+    	goAppoint1($scope.ai2);
+    	break;
+    		case "a3":
+    	goAppoint1($scope.ai3);
+    	break;
+    		case "a4":
+    	goAppoint1($scope.ai4);
+    	break;
+    		case "a5":
+    	goAppoint1($scope.ai5);
+    	break;
+    		case "a6":
+    	goAppoint1($scope.ai6);
+    	break;
+    		case "a7":
+    	goAppoint1($scope.ai7);
+    	break;
+    }
+
+});
+document.getElementById("four").addEventListener("click",function(e) {
+    // 真正的处理过程在这里
+    switch (e.target.parentElement.id){
+	case "n1":
+    	goAppoint1($scope.ni1);
+    	break;
+    	case "n2":
+    	goAppoint1($scope.ni2);
+    	break;
+    	case "n3":
+    	goAppoint1($scope.ni3);
+    	break;
+    	case "n4":
+    	goAppoint1($scope.ni4);
+    	break;
+    	case "n5":
+    	goAppoint1($scope.ni5);
+    	break;
+    	case "n6":
+    	goAppoint1($scope.ni6);
+    	break;
+    	case "n7":
+    	goAppoint1($scope.ni7);
+    	break;
+    }
+
+});
+	}
 	
 	
     function set(){
@@ -75,25 +175,25 @@ app.controller('DoctorDetailCtrl', function($scope, $stateParams,$state, DoctorS
     	 var w6=document.getElementById("w6");
     	 var w7=document.getElementById("w7");
     	 switch($scope.weekindex){
-    	 	case "一":
+    	 	case "周一":
     	 	w1.style.backgroundColor="#ccffdd";
     	 	 break;
-    	 	 case "二":
+    	 	 case "周二":
     	 	w2.style.backgroundColor="#ccffdd";
     	 	 break;
-    	 	 case "三":
+    	 	 case "周三":
     	 	w3.style.backgroundColor="#ccffdd";
     	 	 break;
-    	 	 case "四":
+    	 	 case "周四":
     	 	w4.style.backgroundColor="#ccffdd";
     	 	 break;
-    	 	 case "五":
+    	 	 case "周五":
     	 	w5.style.backgroundColor="#ccffdd";
     	 	 break;
-    	 	 case "六":
+    	 	 case "周六":
     	 	w6.style.backgroundColor="#ccffdd";
     	 	 break;
-    	 	 case "七":
+    	 	 default :
     	 	w7.style.backgroundColor="#ccffdd";
     	 	 break;
     	 }
@@ -105,21 +205,24 @@ app.controller('DoctorDetailCtrl', function($scope, $stateParams,$state, DoctorS
 		$scope.dateList = [];
 		for(var i = 0; i < 7; i++){
 			var da = {};
-			da.date = new Date(sdate.getTime()+i*DAY_Millisecond);
+			da.date = new Date(sdate.getTime()-DAY_Millisecond);
 			var week = da.date.getDay();  
 			var minusDay = week != 0 ? week - 1 : 6;  
+			minusDay=minusDay+1;
 			//周一
 			 da.monday = new Date(sdate.getTime() - (minusDay * DAY_Millisecond));  
            //本周 周日    
-              da.sunday = new Date(da.monday.getTime() + (5 * DAY_Millisecond));  
+              da.sunday = new Date(da.monday.getTime() + (6 * DAY_Millisecond));  
  
 			
-			da.week = weekNames[da.date.getDay()+1];//周几
-			$scope.weekindex=da.week;
-			da.simpleDate = (da.date.getMonth()+1)+'-'+(da.date.getDate());//简单日期‘06-28’
+			da.week = weekNames[da.date.getDay()];//周几
+		
+			da.simpleDate = (da.date.getMonth())+'-'+(da.date.getDate());//简单日期‘06-28’
 			
 			da.simpleFullDate = simplyFormateDate(da.date);//简单全日期‘2016-06-28’
 			if(i==0){
+				$scope.weekindex=da.week;
+			    console.log("----"+$scope.weekindex);
 				$scope.start=simplyFormateDate(da.monday);
 				$scope.start1=simplyFormateDate1(da.monday);
 				$scope.start2=simplyFormateDate1(da.monday);
@@ -181,9 +284,9 @@ app.controller('DoctorDetailCtrl', function($scope, $stateParams,$state, DoctorS
 			
 			$scope.currentDate = da;
 			//UTIL_DIALOG.show($scope.docId+"\n"+sdate+"\n"+edate);
-			//console.log($scope.start);
-			//console.log($scope.end);
-			//console.log($scope.docId);
+			console.log($scope.start);
+			console.log($scope.end);
+			console.log($scope.docId);
 			ArrayJob.arrayJob({
 				docId:$scope.docId,
 				dateStart: $scope.start1,
@@ -205,42 +308,33 @@ app.controller('DoctorDetailCtrl', function($scope, $stateParams,$state, DoctorS
 			for(var i=0;i<arrayJobs.length;i++){
 				var aj = arrayJobs[i];
 				if(aj.regisrationPeriodCd==1){//全班
-					
-					 $scope.fullJobs.push(aj);
-			       //  console.log(selctday(aj.arrayjobDate)+"-111-"+aj.allowReservationNum);
+			      
 				   	addall(selctday(aj.arrayjobDate),aj.allowReservationNum,aj);
-				   
-				}else if(aj.regisrationPeriodCd==2){
-					$scope.moningJobs.push(aj);
-					/// console.log(selctday(aj.arrayjobDate)+"-222-"+aj.allowReservationNum);
-						 addam(selctday(aj.arrayjobDate),aj.allowReservationNum,aj);
-						 	
-				}else if(aj.regisrationPeriodCd==3){
-					$scope.afternoonJobs.push(aj);
-					// console.log(selctday(aj.arrayjobDate)+"-333-"+aj.allowReservationNum);
-						 	addapm(selctday(aj.arrayjobDate),aj.allowReservationNum,aj);
+				   }else if(aj.regisrationPeriodCd==2){
+				
+				
+					addam(selctday(aj.arrayjobDate),aj.allowReservationNum,aj);
+				   }else if(aj.regisrationPeriodCd==3){
+				addapm(selctday(aj.arrayjobDate),aj.allowReservationNum,aj);
 						 	
 				}else{
 					
-					$scope.nightJobs.push(selctday(aj.arrayjobDate),aj);
-					// console.log(selctday(aj.arrayjobDate)+"-444-"+aj.allowReservationNum);
 					addanm(selctday(aj.arrayjobDate),aj.allowReservationNum,aj);
 				}
 			}
 		}
-		$scope.allJobs[0] = $scope.fullJobs;
-		$scope.allJobs[1] = $scope.moningJobs;
-		$scope.allJobs[2] = $scope.afternoonJobs;
-		$scope.allJobs[3] = $scope.nightJobs;
+
 	}
 	//上一页
 	$scope.prex = function(){
 		sevenDays(new Date($scope.dateList[0].date.getTime() - 7 * DAY_Millisecond));
+	    removeview();
 	    getArrayJob();
 	}
 //	//下一页
 	$scope.next = function(){
-		sevenDays(new Date($scope.dateList[6].date.getTime() + DAY_Millisecond));
+		sevenDays(new Date($scope.dateList[6].date.getTime() + 7*DAY_Millisecond));
+	   removeview();
 		getArrayJob();
 		//UTIL_DIALOG.show($scope.arrayJobs);
 	}
@@ -296,8 +390,9 @@ app.controller('DoctorDetailCtrl', function($scope, $stateParams,$state, DoctorS
 	 switch(index1){
 	 	case 1:
 	 	if(allowReservationNum>0){
+	 		q1.innerHTML="";
 	 		q1.innerHTML=s1;
-	 		$scope.q1=aj;
+	 		$scope.qi1=aj;
 	 	}else{
 	 		q1.innerHTML=s2;
 	 	}
@@ -305,43 +400,54 @@ app.controller('DoctorDetailCtrl', function($scope, $stateParams,$state, DoctorS
 	 	break;
 	 	case 2:
 	 	if(allowReservationNum>0){
+	 		q2.innerHTML="";
 	 		q2.innerHTML=s1;
-	 		$scope.q2=aj;
+	 		$scope.qi2=aj;
 	 	}else{
 	 		q2.innerHTML=s2;
 	 	}
 	 	break;
 	 	case 3:
 	 if(allowReservationNum>0){
+	 	    q3.innerHTML="";
 	 		q3.innerHTML=s1;
+	 		$scope.qi3=aj;
 	 	}else{
 	 		q3.innerHTML=s2;
 	 	}
 	 	break;
 	 	case 4:
 	 if(allowReservationNum>0){
+	 	    q4.innerHTML="";
 	 		q4.innerHTML=s1;
+	 		$scope.qi4=aj;
 	 	}else{
 	 		q4.innerHTML=s2;
 	 	}
 	 	break;
 	 	case 5:
 	if(allowReservationNum>0){
+		    q5.innerHTML="";
 	 		q5.innerHTML=s1;
+	 		$scope.qi5=aj;
 	 	}else{
 	 		q5.innerHTML=s2;
 	 	}
 	 	break;
 	 	case 6:
 if(allowReservationNum>0){
+	        q6.innerHTML="";
 	 		q6.innerHTML=s1;
+	 		$scope.qi6=aj;
 	 	}else{
 	 		q6.innerHTML=s2;
 	 	}
 	 	break;
 	 	case 7:
 	if(allowReservationNum>0){
+		    q7.innerHTML="";
 	 		q7.innerHTML=s1;
+	 		$scope.qi7=aj;
 	 	}else{
 	 		q7.innerHTML=s2;
 	 	}
@@ -361,7 +467,9 @@ if(allowReservationNum>0){
 	  switch(index1){
 	 	case 1:
 	 	if(allowReservationNum>0){
+	 		a1.innerHTML="";
 	 		a1.innerHTML=s1;
+	 		$scope.ai1=aj;
 	 	}else{
 	 		a1.innerHTML=s2;
 	 	}
@@ -369,42 +477,54 @@ if(allowReservationNum>0){
 	 	break;
 	 	case 2:
 	 	if(allowReservationNum>0){
+	 		a2.innerHTML="";
 	 		a2.innerHTML=s1;
+	 		$scope.ai2=aj;
 	 	}else{
 	 		a2.innerHTML=s2;
 	 	}
 	 	break;
 	 	case 3:
 	 if(allowReservationNum>0){
+	 	    a3.innerHTML="";
 	 		a3.innerHTML=s1;
+	 		$scope.ai3=aj;
 	 	}else{
 	 		a3.innerHTML=s2;
 	 	}
 	 	break;
 	 	case 4:
 	 if(allowReservationNum>0){
+	 	    a4.innerHTML="";
 	 		a4.innerHTML=s1;
+	 		$scope.ai4=aj;
 	 	}else{
 	 		a4.innerHTML=s2;
 	 	}
 	 	break;
 	 	case 5:
 	if(allowReservationNum>0){
+		    a5.innerHTML="";
 	 		a5.innerHTML=s1;
+	 		$scope.ai5=aj;
 	 	}else{
 	 		a5.innerHTML=s2;
 	 	}
 	 	break;
 	 	case 6:
 if(allowReservationNum>0){
+	        a6.innerHTML="";
 	 		a6.innerHTML=s1;
+	 		$scope.ai6=aj;
 	 	}else{
 	 		a6.innerHTML=s2;
 	 	}
 	 	break;
 	 	case 7:
 	if(allowReservationNum>0){
+		    a7.innerHTML="";
 	 		a7.innerHTML=s1;
+	 		$scope.ai7=aj;
 	 	}else{
 	 		a7.innerHTML=s2;
 	 	}
@@ -413,6 +533,7 @@ if(allowReservationNum>0){
 	}
 	//添加下午
 	function addapm(index1,allowReservationNum,aj){
+	
      var p1=document.getElementById("p1");
 	 var p2=document.getElementById("p2");
 	 var p3=document.getElementById("p3");
@@ -420,10 +541,14 @@ if(allowReservationNum>0){
 	 var p5=document.getElementById("p5");
 	 var p6=document.getElementById("p6");
 	 var p7=document.getElementById("p7");
+	 
 	  switch(index1){
+	  
 	 	case 1:
 	 	if(allowReservationNum>0){
+	 		p1.innerHTML="";
 	 		p1.innerHTML=s1;
+	 		$scope.pi1=aj;
 	 	}else{
 	 		p1.innerHTML=s2;
 	 	}
@@ -431,22 +556,25 @@ if(allowReservationNum>0){
 	 	break;
 	 	case 2:
 	 	if(allowReservationNum>0){
+	 		p2.innerHTML="";
 	 		p2.innerHTML=s1;
+	 		$scope.pi2=aj;
 	 	}else{
 	 		p2.innerHTML=s2;
 	 	}
 	 	break;
 	 	case 3:
 	 if(allowReservationNum>0){
-	        
+	        p3.innerHTML="";
 	 		p3.innerHTML=s1;
-	 	
+	 	 	$scope.pi3=aj;
 	 	}else{
 	 		p3.innerHTML=s2;
 	 	}
 	 	break;
 	 	case 4:
 	 if(allowReservationNum>0){
+	 	    p4.innerHTML="";
 	 		p4.innerHTML=s1;
 	         $scope.pi4=aj;
 	 	}else{
@@ -455,21 +583,27 @@ if(allowReservationNum>0){
 	 	break;
 	 	case 5:
 	if(allowReservationNum>0){
+		    p5.innerHTML="";
 	 		p5.innerHTML=s1;
+	 		 $scope.pi5=aj;
 	 	}else{
 	 		p5.innerHTML=s2;
 	 	}
 	 	break;
 	 	case 6:
 if(allowReservationNum>0){
+	        p6.innerHTML="";
 	 		p6.innerHTML=s1;
+	 		$scope.pi6=aj;
 	 	}else{
 	 		p6.innerHTML=s2;
 	 	}
 	 	break;
 	 	case 7:
 	if(allowReservationNum>0){
+		    p7.innerHTML="";
 	 		p7.innerHTML=s1;
+	 		$scope.pi7=aj;
 	 	}else{
 	 		p7.innerHTML=s2;
 	 	}
@@ -488,7 +622,9 @@ if(allowReservationNum>0){
 	  switch(index1){
 	 	case 1:
 	 	if(allowReservationNum>0){
+	       	n1.innerHTML="";
 	 		n1.innerHTML=s1;
+	 		$scope.ni1=aj;
 	 	}else{
 	 		n1.innerHTML=s2;
 	 	}
@@ -496,47 +632,91 @@ if(allowReservationNum>0){
 	 	break;
 	 	case 2:
 	 	if(allowReservationNum>0){
+	 		n2.innerHTML="";
 	 		n2.innerHTML=s1;
+	 		$scope.ni2=aj;
 	 	}else{
 	 		n2.innerHTML=s2;
 	 	}
 	 	break;
 	 	case 3:
 	 if(allowReservationNum>0){
+	 		n3.innerHTML="";
 	 		n3.innerHTML=s1;
+	 		$scope.ni3=aj;
 	 	}else{
 	 		n3.innerHTML=s2;
 	 	}
 	 	break;
 	 	case 4:
 	 if(allowReservationNum>0){
+	 		n4.innerHTML="";
 	 		n4.innerHTML=s1;
+	 		$scope.ni4=aj;
 	 	}else{
 	 		n4.innerHTML=s2;
 	 	}
 	 	break;
 	 	case 5:
 	if(allowReservationNum>0){
+		   n5.innerHTML="";
 	 		n5.innerHTML=s1;
+	 		$scope.ni5=aj;
 	 	}else{
 	 		n5.innerHTML=s2;
 	 	}
 	 	break;
 	 	case 6:
 if(allowReservationNum>0){
+	       n6.innerHTML="";
 	 		n6.innerHTML=s1;
+	 		$scope.ni6=aj;
 	 	}else{
 	 		n6.innerHTML=s2;
 	 	}
 	 	break;
 	 	case 7:
 	if(allowReservationNum>0){
+			n7.innerHTML="";
 	 		n7.innerHTML=s1;
+	 		$scope.ni7=aj;
 	 	}else{
 	 	    n7.innerHTML=s2;
 	 	}
 	 	break;
 	 }
+	}
+	function removeview(){
+		document.getElementById("q1").innerHTML="";
+		document.getElementById("q2").innerHTML="";
+		document.getElementById("q3").innerHTML="";
+		document.getElementById("q4").innerHTML="";
+		document.getElementById("q5").innerHTML="";
+		document.getElementById("q6").innerHTML="";
+		document.getElementById("q7").innerHTML="";
+		document.getElementById("a1").innerHTML="";
+	    document.getElementById("a2").innerHTML="";
+	    document.getElementById("a3").innerHTML="";
+		document.getElementById("a4").innerHTML="";
+		document.getElementById("a5").innerHTML="";
+		document.getElementById("a6").innerHTML="";
+		document.getElementById("a7").innerHTML="";
+		document.getElementById("p1").innerHTML="";
+		document.getElementById("p2").innerHTML="";
+		document.getElementById("p3").innerHTML="";
+		document.getElementById("p4").innerHTML="";
+		document.getElementById("p5").innerHTML="";
+		document.getElementById("p6").innerHTML="";
+	    document.getElementById("p7").innerHTML="";
+	    document.getElementById("n1").innerHTML="";
+	    document.getElementById("n2").innerHTML="";
+	    document.getElementById("n3").innerHTML="";
+	    document.getElementById("n4").innerHTML="";
+	    document.getElementById("n5").innerHTML="";
+	    document.getElementById("n6").innerHTML="";
+	    document.getElementById("n7").innerHTML="";
+	    
+	    
 	}
 	
 
