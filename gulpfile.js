4313e8c27f37d3472ed4bacb970946b27626af57
李志勇 <lizhiyong@sinosoft.com.cn>
2016-10-17 19:32:48 +0800

    调整项目目录结构

--- gulpfile.js
+++ gulpfile.js
@@ -8,20 +8,27 @@ var useref = require('gulp-useref');
 var gulpIf = require('gulp-if');
 var minifyCSS = require('gulp-minify-css');
 var runSequence = require('run-sequence');
+var imagemin = require('gulp-imagemin');
+var cache = require('gulp-cache');
 
 // 配置项
 var paths = {
-	build: 'www/js/build',
-	src_js: ['www/js/src/**/*.js'],
-	src_template: ['www/js/src/**/*.html']
+	build: 'www',
+	src_js: ['static/js/src/**/*.js'],
+	src_img: ['static/img/**/*'],
+	src_font: ['static/lib/ionic/fonts/*.*'],
+	src_template: ['static/js/src/**/*.html'],
+	src_index_html: "static/index.html"
 };
 
 // 默认任务
-gulp.task('default', ['minify', 'watch']);
+gulp.task('default', ['build', 'watch']);
 
 // 压缩所有相关文件
-gulp.task('minify', function (){
-	runSequence("minifyJs", "templatecache", "useref")
+gulp.task('build', function (){
+	runSequence(
+		"minifyJs", "minifyImg", "copyFont",
+		"templatecache", "useref")
 });
 // js 合并压缩
 gulp.task('minifyJs', function (done){
@@ -31,6 +38,21 @@ gulp.task('minifyJs', function (done){
 		//.pipe(uglify())
 		//.pipe(rename('main.min.js'))
 		//.pipe(gulp.dest(paths.build))
+		.on('end', done)
+		;
+});
+// 图片压缩
+gulp.task('minifyImg', function (done){
+	 gulp.src(paths.src_img)
+		.pipe(cache(imagemin({optimizationLevel: 3, progressive: true, interlaced: true })))
+		.pipe(gulp.dest(paths.build + "/img"))
+		.on('end', done)
+		;
+});
+// 拷贝字体文件
+gulp.task('copyFont', function (done){
+	 gulp.src(paths.src_font)
+		.pipe(gulp.dest(paths.build + "/fonts"))
 		.on('end', done)
 		;
 });
@@ -44,7 +66,7 @@ gulp.task('templatecache', function (done) {
 });
 // 将页面中引用的文件合并并压缩
 gulp.task('useref', function (done) {
-	gulp.src("www/index.html")
+	gulp.src(paths.src_index_html)
 		.pipe(gulpIf('*.css', minifyCSS()))
 		.pipe(gulpIf('*.js', uglify()))
 		.pipe(useref())
@@ -56,6 +78,7 @@ gulp.task('useref', function (done) {
 gulp.task('watch', ['minify', 'templatecache'], function() {
 	gulp.watch(paths.src_js, ['minify']);
 	gulp.watch(paths.src_template, ['minify']);
+	gulp.watch(paths.src_img, ['minify']);
 });
 // 清空发布
 gulp.task('clean', function () {
--- static/js/src/routes.js
+++ static/js/src/routes.js
@@ -10,7 +10,7 @@ angular.module('app.routes', [])
 	//主页-首页
 	.state('index', {
 		url: '/index',
-		templateUrl: 'js/src/index/views/main_index.html',
+		templateUrl: 'index/views/main_index.html',
 		controller: 'MainIndexCtrl'
 	})
 
@@ -19,56 +19,56 @@ angular.module('app.routes', [])
 		url: '/hospital',
 		cache: false,
 		params: {hos: null, hosId: null},
-		templateUrl: 'js/src/hospital/views/detail.html',
+		templateUrl: 'hospital/views/detail.html',
 		controller: 'HosDetailCtrl'
 	})
 	//医院特色科室
 	.state('hosDeptSpe', {
 		url: '/hosDeptSpe',
 		params: {hos: null},
-		templateUrl: 'js/src/hospital/views/hos_dept_spe.html',
+		templateUrl: 'hospital/views/hos_dept_spe.html',
 		controller: "HosDeptSpeCtrl"
 	})
 	//医院专家介绍
 	.state('hosDoctors', {
 		url: '/hosDoctors',
 		params: {hos: null},
-		templateUrl: 'js/src/hospital/views/hos_doctors.html',
+		templateUrl: 'hospital/views/hos_doctors.html',
 		controller: "HosDoctorsCtrl"
 	})
 	//医院科室详情
 	.state('hosDeptDetail', {
 		url: '/hosDeptDetail',
 		params: {dept: null, hos: null},
-		templateUrl: 'js/src/hospital/views/hos_dept_detail.html',
+		templateUrl: 'hospital/views/hos_dept_detail.html',
 		controller: "HosDeptDetailCtrl"
 	})
 	//医院导航
 	.state('hosNav', {
 		url: '/hosNav',
 		params: {hos: null, hosId: null},
-		templateUrl: 'js/src/hospital/views/hos_nav.html',
+		templateUrl: 'hospital/views/hos_nav.html',
 		controller: "HosNavCtrl"
 	})
 	//医院叫号跟踪
 	.state('hosCallNo', {
 		url: '/hosCallNo',
 		params: {hos: null},
-		templateUrl: 'js/src/hospital/views/hos_callno.html',
+		templateUrl: 'hospital/views/hos_callno.html',
 		controller: "HosCallnoCtrl"
 	})
 	//医院院内导航
 	.state('hosInnerNav', {
 		url: '/hosInnerNav',
 		params: {hos: null,},
-		templateUrl: 'js/src/hospital/views/hos_inner_nav.html',
+		templateUrl: 'hospital/views/hos_inner_nav.html',
 		controller: "HosInnerNavCtrl"
 	})
 	//医院院内导航详情
 	.state('hosInnerNavDetail', {
 		url: '/hosInnerNavDetail',
 		params: {nav: null, hos: null},
-		templateUrl: 'js/src/hospital/views/hos_inner_nav_detail.html',
+		templateUrl: 'hospital/views/hos_inner_nav_detail.html',
 		controller: "HosInnerNavDetailCtrl"
 	})
 	//医院价格查询
@@ -76,14 +76,14 @@ angular.module('app.routes', [])
 		url: '/hosPriceSearch',
 		params: {hos: null},
 		cache: false,
-		templateUrl: 'js/src/hospital/views/hos_price_search.html',
+		templateUrl: 'hospital/views/hos_price_search.html',
 		controller: "HosPriceSearchCtrl"
 	})
 	//医院切换、医院选择页面
 	.state('hosSearch', {
 		url: '/hosSearch',
 		params: {targetState: "hospital"},
-		templateUrl: 'js/src/hospital/views/search_hos.html',
+		templateUrl: 'hospital/views/search_hos.html',
 		controller: "SearchHosCtrl"
 	})
 
@@ -92,7 +92,7 @@ angular.module('app.routes', [])
 		url: '/drugPriceSearch',
 		params: {hos: null},
 		cache: false,
-		templateUrl: 'js/src/hospital/views/search_drugprice.html',
+		templateUrl: 'hospital/views/search_drugprice.html',
 		controller: "DrugPriceSearchCtrl"
 	})
 	//服务项目价格查询
@@ -100,7 +100,7 @@ angular.module('app.routes', [])
 		url: '/servItemPriceSearch',
 		params: {hos: null},
 		cache: false,
-		templateUrl: 'js/src/hospital/views/search_servitemprice.html',
+		templateUrl: 'hospital/views/search_servitemprice.html',
 		controller: "ServItemPriceSearchCtrl"
 	})
 
@@ -108,14 +108,14 @@ angular.module('app.routes', [])
 	.state('doctorSearch', {
 		url: '/doctorSearch',
 		params: {targetState: "doctorDetail"},
-		templateUrl: 'js/src/doctor/views/doctor_search.html',
+		templateUrl: 'doctor/views/doctor_search.html',
 		controller: "SearchDoctorCtrl"
 	})
 	//医生主页面
 	.state('doctorDetail', {
 		url: '/doctorDetail',
 		params: {doctor: null, doctorId: null},
-		templateUrl: 'js/src/doctor/views/doctor_detail.html',
+		templateUrl: 'doctor/views/doctor_detail.html',
 		controller: "DoctorDetailCtrl"
 	})
 
@@ -124,48 +124,48 @@ angular.module('app.routes', [])
 		url: '/medicalrecored',
 		params: {patient: null, patientId: null},
 		cache: false,
-		templateUrl: 'js/src/medicalrecoreds/views/main_medicalrecored.html',
+		templateUrl: 'medicalrecoreds/views/main_medicalrecored.html',
 		controller: 'MainMedicalrecordCtrl'
 	})
 	//就诊明细
 	.state('medicalrecoredDetail', {
 		url: '/medicalrecoredDetail/:id',
-		templateUrl: 'js/src/medicalrecoreds/views/medicalrecord_detail.html',
+		templateUrl: 'medicalrecoreds/views/medicalrecord_detail.html',
 		controller: 'MedicalrecordDetailCtrl'
 	})
 	//门诊-处方明细
 	.state('medicalrecoredPre', {
 		url: '/medicalrecoredPre/:idNo',
 		params: {idNo: null, hosOrgCode: null, preNo: null},
-		templateUrl: 'js/src/medicalrecoreds/views/medicalrecord_pre.html',
+		templateUrl: 'medicalrecoreds/views/medicalrecord_pre.html',
 		controller: 'MedicalrecordPreCtrl'
 	})
 	//门诊-医疗费用
 	.state('medicalrecoredFee', {
 		url: '/medicalrecoredFee/:idNo',
 		params: {idNo: null, hosOrgCode: null},
-		templateUrl: 'js/src/medicalrecoreds/views/medicalrecord_fee.html',
+		templateUrl: 'medicalrecoreds/views/medicalrecord_fee.html',
 		controller: 'MedicalrecordFeeCtrl'
 	})
 	//门诊-报告查询
 	.state('medicalrecoredReport', {
 		url: '/medicalrecoredReport/:idNo',
 		params: {idNo: null, hosOrgCode: null},
-		templateUrl: 'js/src/medicalrecoreds/views/medicalrecord_report.html',
+		templateUrl: 'medicalrecoreds/views/medicalrecord_report.html',
 		controller: 'MedicalrecordReportCtrl'
 	})
 
 	//主页-资讯
 	.state('news', {
 		url: '/news',
-		templateUrl: 'js/src/news/views/main_news.html',
+		templateUrl: 'news/views/main_news.html',
 		controller: 'MainNewsCtrl'
 	})
 	//资讯-详情
 	.state('newsDetail', {
 		url: '/detail/:newsId',
 		params: {newsId: null},
-		templateUrl: 'js/src/news/views/news_detail.html',
+		templateUrl: 'news/views/news_detail.html',
 		controller: 'NewsDetailCtrl'
 	})
 
@@ -173,39 +173,39 @@ angular.module('app.routes', [])
 	.state('my', {
 		url: '/my',
 		cache: false,
-		templateUrl: 'js/src/user/views/main_my.html',
+		templateUrl: 'user/views/main_my.html',
 		controller: 'MainMyCtrl'
 	})
-	
+
 	//健康档案
 	.state('healthRecord', {
 		url: '/my',
 		cache: false,
-		templateUrl: 'js/src/user/views/health_record.html',
+		templateUrl: 'user/views/health_record.html',
 		controller: 'HealthRecordCtrl'
 	})
-	
-	
+
+
 	//就诊人-列表
 	.state('patientList', {
 		url: '/patient-list',
 		params: {targetState: "patientEdit",otherParams:null},
 		cache: false,
-		templateUrl: 'js/src/user/views/patient_list.html',
+		templateUrl: 'user/views/patient_list.html',
 		controller: 'PatientListCtrl'
 	})
 	//就诊人-编辑
 	.state('patientEdit', {
 		url: '/patient-edit',
 		params: {patient: null},
-		templateUrl: 'js/src/user/views/patient_edit.html',
+		templateUrl: 'user/views/patient_edit.html',
 		controller: 'PatientEditCtrl'
 	})
 	//添加就诊人
 	.state('patientadd', {
 		url: '/patient-add',
 		//params: {patient: null},
-		templateUrl: 'js/src/user/views/add_patient.html',
+		templateUrl: 'user/views/add_patient.html',
 		controller: 'PatientEditCtrl'
 	})
 	//就医卡列表
@@ -213,7 +213,7 @@ angular.module('app.routes', [])
 		url: '/mymedicalcardlist',
 		params: {targetState: "cardpay",otherParams:null},
 		cache: false,
-		templateUrl: 'js/src/user/views/mymedicalcard_list.html',
+		templateUrl: 'user/views/mymedicalcard_list.html',
 		controller: 'MyCardCtrl'
 	})
 	//就医卡消费
@@ -221,49 +221,49 @@ angular.module('app.routes', [])
 		url: '/cardpay',
 		params: {card: null},
 		cache:false,
-		templateUrl: 'js/src/user/views/cardpay.html',
+		templateUrl: 'user/views/cardpay.html',
 		controller: 'CardpayCtrl'
 	})
 	//添加就医卡
 	.state('addcard', {
 		url: '/addcard',
 		params: {hos: null},
-		templateUrl: 'js/src/user/views/add_card.html',
+		templateUrl: 'user/views/add_card.html',
 		controller: 'addcardctrl'
 	})
 	//关于我们
 	.state('aboutUs', {
 		url: '/aboutUs',
-		templateUrl: 'js/src/system/views/about_us.html'
+		templateUrl: 'system/views/about_us.html'
 	})
 	//功能介绍
 	.state('introduction', {
 		url: '/introduction',
-		templateUrl: 'js/src/system/views/introduction.html'
+		templateUrl: 'system/views/introduction.html'
 	})
 	//意见反馈
 	.state('opinion', {
 		url: '/opinion',
-		templateUrl: 'js/src/user/views/opinion.html',
+		templateUrl: 'user/views/opinion.html',
 		controller: 'OpinionCtrl'
 	})
 	//药房用药须知
 	.state('hosDrugNote', {
 		url: '/hosDrugNote',
-		templateUrl: 'js/src/system/views/hos_drug_note.html'
+		templateUrl: 'system/views/hos_drug_note.html'
 	})
 
 	//登录页
 	.state('login', {
 		url: '/login',
 		params: {from: null, fromParams: null, to: null, toParams: null},
-		templateUrl: 'js/src/user/views/login.html',
+		templateUrl: 'user/views/login.html',
 		controller: 'LoginCtrl'
 	})
 	//注册页
 	.state('signup', {
 		url: '/signup',
-		templateUrl: 'js/src/user/views/signup.html',
+		templateUrl: 'user/views/signup.html',
 		controller: 'SignupCtrl'
 	})
 
@@ -271,21 +271,21 @@ angular.module('app.routes', [])
 	.state('messagelist', {
 		url: '/messages',
 		cache: false,
-		templateUrl: 'js/src/message/views/messages.html',
+		templateUrl: 'message/views/messages.html',
 		controller: 'MessagesListCtrl'
 	})
 	//消息详情页
 	.state('messagedetail', {
 		url: '/messagedetail',
 		params: {message: null},
-		templateUrl: 'js/src/message/views/message_detail.html',
+		templateUrl: 'message/views/message_detail.html',
 		controller: 'MessagesDetailCtrl'
 	})
 
 	//搜索医院页
 	.state('searchHosAndDoc', {
 		url: '/searchHosAndDoc',
-		templateUrl: 'js/src/hospital/views/search_hosanddoc.html',
+		templateUrl: 'hospital/views/search_hosanddoc.html',
 		controller: 'SearchHosAndDocCtrl'
 	})
 
@@ -293,73 +293,73 @@ angular.module('app.routes', [])
 	.state('appointQuick', {
 		url: '/appointQuick',
 		params: {zone: null, hos: null},
-		templateUrl: 'js/src/appoint/views/appoint_quick.html',
+		templateUrl: 'appoint/views/appoint_quick.html',
 		controller: "AppointQuickCtrl"
 	})
 	//预约详情页
 	.state('appointDetail', {
 		url: '/appointDetail',
 		params: {appointId: null},
-		templateUrl: 'js/src/appoint/views/appoint_detail.html',
+		templateUrl: 'appoint/views/appoint_detail.html',
 		controller: "AppointDetailCtrl"
 	})
 	//预约挂号确认页
 	.state('appointMent',{
 		params:{aj:null, patient:null, otherParams:null},
 		url:'/appointMent',
-		templateUrl:'js/src/appoint/views/appoint_ment.html',
+		templateUrl:'appoint/views/appoint_ment.html',
 		controller:'AppointMentCtrl'
 	})
 	//预约选医院页
 	.state('appointHos',{
 		url:'/appointHos',
-		templateUrl:'js/src/appoint/views/appoint_hos.html',
+		templateUrl:'appoint/views/appoint_hos.html',
 		controller:'AppointHosCtrl'
 	})
 	//预约选科室
 	.state('appointDepart',{
 		url:'/appointDepart',
 		params:{targetState: "appointDate", hos: null},
-		templateUrl:'js/src/appoint/views/appoint_depart.html',
+		templateUrl:'appoint/views/appoint_depart.html',
 		controller:'AppointDepartCtrl'
 	})
 	//预约选日期及时间段
 	.state('appointDate',{
 		url:'/appointDate',
 		params:{dept: null, hos:null},
-		templateUrl:'js/src/appoint/views/appoint_date.html',
+		templateUrl:'appoint/views/appoint_date.html',
 		controller:'AppointDateCtrl'
 	})
 	//预约详情页
 	.state('appointMentDatail',{
 		url:'/appointMentDatail',
-		templateUrl:'js/src/appoint/views/appointment_datail.html',
+		templateUrl:'appoint/views/appointment_datail.html',
 		controller:'AppointMentDatailCtrl'
 	})
 	//预约成功页
 	.state('appointSuccess',{
 		params:{appointId:null},
 		url:'/appointSuccess',
-		templateUrl:'js/src/appoint/views/appoint_success.html',
+		templateUrl:'appoint/views/appoint_success.html',
 		controller:'AppointSuccessCtrl'
 	})
 	//预约列表
 	.state('appointList',{
 		url:'/appointList',
-		templateUrl:'js/src/appoint/views/appoint_list.html',
+		templateUrl:'appoint/views/appoint_list.html',
 		controller:'AppointListCtrl'
 	})
 	//设置服药提醒
 	.state('drugAlert',{
 		url:'/drugAlert',
-		templateUrl:'js/src/system/views/drug_alert.html',
+		templateUrl:'system/views/drug_alert.html',
 		controller:'DrugAlertCtrl'
 	})
 	//就医评价
 	.state('outEvaluate',{
 		url:'/outEvaluate',
 		params: {hos: null, clinNo: null},
-		templateUrl:'js/src/system/views/out_evaluate.html',
+		templateUrl:'system/views/out_evaluate.html',
 		controller:'OutEvaluateCtrl'
 	})
 
